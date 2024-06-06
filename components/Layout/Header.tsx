33ddd9200b53bc5a6b8c6948af612febb2f0cfa5
Suman9865 <93024382+Suman9865@users.noreply.github.com>
2024-06-06 12:11:30 +0530

    Update Header.tsx
    Changes made

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
     <>
       <nav className="w-full flex items-center justify-between h-20 px-20 backdrop-blur-xl bg-white/30 sticky top-0 drop-shadow-md z-50">
         <h2 className="md:text-5xl text-3xl font-bold text-[#262626]">
-          Flone.
+         DIGISTORE
         </h2>
         <div className="md:flex items-center justify-center font-bold hidden">
         <HeaderNavigationMenu />
